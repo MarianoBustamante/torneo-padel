eb9cb08abe12265e005db14a03803abf86c4ac35
mariano <marianobustamante111@gmail.com>
2025-02-03 00:38:28 -0300

    se agrego la funcion para editar o eliminar jugadores

--- jugadores/urls.py
+++ jugadores/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
-from .views import lista_jugadores
-from .views import agregar_jugador
+from .views import lista_jugadores, agregar_jugador, editar_jugador, eliminar_jugador
 
 urlpatterns = [
     path('', lista_jugadores, name='lista_jugadores'),
     path('agregar/', agregar_jugador, name='agregar_jugador'),
-    
+    path('editar/<int:jugador_id>/', editar_jugador, name='editar_jugador'),
+    path('eliminar/<int:jugador_id>/', eliminar_jugador, name='eliminar_jugador'),
 ]
--- jugadores/views.py
+++ jugadores/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Jugador
 from .forms import JugadorForm
 
@@ -16,3 +16,21 @@ def agregar_jugador(request):
         form = JugadorForm()
     
     return render(request, 'jugadores/agregar.html', {'form': form})
+
+def editar_jugador(request, jugador_id):
+    jugador = get_object_or_404(Jugador, id=jugador_id)
+    if request.method == 'POST':
+        jugador.delete()
+        return redirect('lista_jugadores')
+    else:
+        form = JugadorForm(instance=jugador)
+        
+    return render(request, 'jugadores/editar.html', {'form': form, 'jugador': jugador})    
+
+def eliminar_jugador(request, jugador_id):
+    jugador = get_object_or_404(Jugador, id=jugador_id)
+    if request.method == 'POST':
+        jugador.delete()
+        return redirect('lista_jugadores')
+    
+    return render(request, 'jugadores/eliminar.html', {'jugador': jugador})
